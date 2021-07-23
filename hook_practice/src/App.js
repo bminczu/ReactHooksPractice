89ab182f67b713a347cdc4cda5dc8e9c84b6ce16
bminczu <bminczu@gmail.com>
2021-07-23 12:21:06 -0500

    modifying state not replacing

--- hook_practice/src/App.js
+++ hook_practice/src/App.js
@@ -5,18 +5,31 @@ import React, {useState} from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(89)
+  const [state, setState] = useState({count: 55, theme: "dark"})
+  const count = state.count
+  const theme = state.theme
+
 
   function decrementCount(){
-    setCount(count-1)
+    setState(prevState => {
+      return {...prevState, count: prevState.count - 1}
+    })
+    // setCount(prevCount => prevCount - 1)
   }
 
+  function incrementCount(){
+    setState(prevState => {
+      return {...prevState, count: prevState + 1}
+    })
+  //  setCount(prevCount => prevCount + 1)
+  }
   return(
 
     <div>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
-      <button>+</button>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
 
     </div>
   )
