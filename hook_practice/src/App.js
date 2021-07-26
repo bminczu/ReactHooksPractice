0697fbedfbbcbea3f044cb97aa862abdd344cf33
bminczu <bminczu@gmail.com>
2021-07-26 15:36:26 -0500

    useEffect Practice start

--- hook_practice/src/App.js
+++ hook_practice/src/App.js
@@ -1,35 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 
 
 function App() {
 
-  const [count, setCount] = useState(0)
-  const [theme, setTheme] = useState('dark')
+  const [resourceType, setResourceType] = useState('posts')
 
-  //if you don't want your state "clashing" separate them
+  useEffect(()=> {
+    console.log('render')
+  }, [resourceType])
+  
 
-
-
-  function decrementCount(){
-    setCount(prevCount => prevCount -1)
-  }
-
-  function incrementCount(){
-   setCount(prevCount => prevCount + 1)
-   setTheme('light')
-  //  setCount(prevCount => prevCount + 1)
-  }
   return(
 
+    <>
     <div>
-      <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <span>{theme}</span>
-      <button onClick={incrementCount}>+</button>
-
+      <button onClick={()=> setResourceType('posts')}>Posts</button>
+      <button onClick={()=>setResourceType('users')}>Users</button>
+      <button onClick={()=> setResourceType('comments')}>Comments</button>
     </div>
+    <h1>{resourceType}</h1>
+    </>
   )
 }
 
